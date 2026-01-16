bf821a065c61a99cf4a1c7995c92932ef401aac9
WeAreCode045 <maurice@code045.nl>
2026-01-16 21:26:25 +0100

    Add main plugin file with initial setup and functionality includes

---
+++ wp-dev-tools.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Plugin Name:       WP Dev Tools
+ * Plugin URI:        https://code045.nl/plugins/the-basics/
+ * Description:       A collection of useful tools for WordPress development and management.
+ * Version:           1.0.1
+ * Author:            Code045
+ * Author URI:        https://code045.nl/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       wp-dev-tools
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
+
+define( 'WP_DEV_TOOLS_VERSION', '1.0.0' );
+
+// Include the lifecycle functions.
+require_once plugin_dir_path( __FILE__ ) . 'includes/lifecycle.php';
+register_activation_hook( __FILE__, 'wp_dev_tools_activate' );
+register_deactivation_hook( __FILE__, 'wp_dev_tools_deactivate' );
+
+
+// Include the admin settings page.
+require_once plugin_dir_path( __FILE__ ) . 'admin/settings.php';
+
+// Include the notifications functionality.
+require_once plugin_dir_path( __FILE__ ) . 'includes/notifications.php';
+
+// Include the remote library functionality.
+require_once plugin_dir_path( __FILE__ ) . 'includes/remote-library.php';
